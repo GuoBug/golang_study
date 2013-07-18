fd1a8396f19ec426d0f7e4801c16cc5657b8e391
unknown <zhanggggfd@gmail.com>
2013-07-18 22:12:15 +0800

    modified:   bubble_sort.go

--- bubble_sort.go
+++ bubble_sort.go
@@ -1,19 +1,21 @@
-package main 
+package main
 
 import (
 	"fmt"
 )
 
 func main() {
-	array := [10]int {14,24,35,46,2,61,73,25,725,43}
+	/* 冒泡排序
+	 * 循环运用
+	 */
+	array := [10]int{14, 24, 35, 46, 2, 61, 73, 25, 725, 43}
 
 	fmt.Print("数组为")
 	fmt.Println(array)
 
-
 	for i := 0; i < 10; i++ {
 		for j := i + 1; j < 10; j++ {
-			if array[i] < array[j]{
+			if array[i] < array[j] {
 				array[i] = array[i] + array[j]
 				array[j] = array[i] - array[j]
 				array[i] = array[i] - array[j]
